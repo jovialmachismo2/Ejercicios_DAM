3667e502c8cbfa31ada128f93f1afe2d7c1c35a8
A704\dam1 <joaquinmagnoli10@gmail.com>
2025-11-05 21:19:51 +0100

    casi

---
+++ ejercicios/src/main/java/org/example/Tema3_bateria_Arrays5.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Tema3_bateria_Arrays5 {
+    static void main() {
+
+
+        int[] vector = {1, 2, 3, 4, 5};
+        int[] filtrado = new int[vector.length];
+        int aux = vector[vector.length-1];
+
+        for (int i = 0; i < filtrado.length-1; i++) {
+            if (i == 0){
+                filtrado[0] = aux;
+            }else{
+                filtrado[i] = vector[i-1];
+            }
+
+        }
+        System.out.println(Arrays.toString(filtrado));
+    }
+}
